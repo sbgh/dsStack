8ab099fd05f0f656eccc68633660a924635913eb
Scott Hurd <scott.hurd@cybera.ca>
2024-03-19 17:02:04 +0000

    fix

--- upgrade.sh
+++ upgrade.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "stop dsStack"
-sudo pm2 stop dsStack
+# sudo pm2 stop dsStack
 sudo curl https://raw.githubusercontent.com/cybera/dsStack/master/Server.js -o Server.js
 sudo sed -i 's|/etc/ssl/private|/etc/letsencrypt/live/dsstack.cybera.ca|g' Server.js
-sudo pm2 start dsStack
+sudo pm2 restart dsStack
