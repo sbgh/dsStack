a3394c91df3218ce3f0cb13f804e848bad6cb88a
Scott Hurd <scott.hurd@cybera.ca>
2024-03-19 16:47:00 +0000

    fix ssh pem locations

--- upgrade.sh
+++ upgrade.sh
@@ -3,4 +3,5 @@
 echo "stop dsStack"
 sudo pm2 stop dsStack
 sudo curl https://raw.githubusercontent.com/cybera/dsStack/master/Server.js -o Server.js
+sudo sed -i 's|/etc/ssl/private|/etc/letsencrypt/live/dsstack.cybera.ca|g' Server.js
 sudo pm2 start dsStack
